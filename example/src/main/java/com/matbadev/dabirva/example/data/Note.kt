385450a985ba23c080e50719810e22024f54d45c
Matthias Bäuerle <Ma.Baeuerle@gmx.de>
2021-01-09 11:21:40 +0100

    Add colors to notes in example

--- example/src/main/java/com/matbadev/dabirva/example/data/Note.kt
+++ example/src/main/java/com/matbadev/dabirva/example/data/Note.kt
@@ -1,7 +1,10 @@
 package com.matbadev.dabirva.example.data
 
+import androidx.annotation.ColorInt
+
 data class Note(
     val id: Long,
     val text: String,
+    @ColorInt val color: Int,
     val priority: NotePriority,
 )
--- example/src/main/java/com/matbadev/dabirva/example/data/NoteRepository.kt
+++ example/src/main/java/com/matbadev/dabirva/example/data/NoteRepository.kt
@@ -2,8 +2,23 @@ package com.matbadev.dabirva.example.data
 
 class NoteRepository {
 
+    companion object {
+        private val MATERIAL_COLORS_100: List<Int> = listOf(
+            0xffcfd8dc.toInt(),
+            0xffffcdd2.toInt(),
+            0xffe1bee7.toInt(),
+            0xffc5cae9.toInt(),
+            0xffb3e5fc.toInt(),
+            0xffb2dfdb.toInt(),
+            0xffdcedc8.toInt(),
+            0xfffff9c4.toInt(),
+            0xffffe0b2.toInt(),
+            0xffd7ccc8.toInt(),
+        )
+    }
+
     fun getNotes(): List<Note> {
-        return generateNotes(10)
+        return generateNotes(MATERIAL_COLORS_100.size)
     }
 
     private fun generateNotes(notesPerPriority: Int): List<Note> {
@@ -37,6 +52,7 @@ class NoteRepository {
             return Note(
                 id = noteId,
                 text = "Note #$noteId",
+                color = MATERIAL_COLORS_100[noteId.toInt() % MATERIAL_COLORS_100.size],
                 priority = priority,
             )
         }
