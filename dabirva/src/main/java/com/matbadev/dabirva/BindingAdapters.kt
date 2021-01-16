b9a415d8513c12c5c921b4ef49950511e0ad9c84
Matthias Bäuerle <Ma.Baeuerle@gmx.de>
2021-01-17 00:35:56 +0100

    Require DabirvaData in binding adapter

--- dabirva/src/main/java/com/matbadev/dabirva/BindingAdapters.kt
+++ dabirva/src/main/java/com/matbadev/dabirva/BindingAdapters.kt
@@ -4,11 +4,14 @@ import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 
 @BindingAdapter("dabirvaData")
-fun setDabirvaData(recyclerView: RecyclerView, dabirvaData: DabirvaData?) {
-    if (dabirvaData == null) return
-    when (val currentAdapter: RecyclerView.Adapter<*>? = recyclerView.adapter) {
-        null -> recyclerView.adapter = Dabirva(dabirvaData)
-        is Dabirva -> currentAdapter.data = dabirvaData
-        else -> throw IllegalStateException("Different adapter already set")
+@Suppress("CascadeIf")
+fun setDabirvaData(recyclerView: RecyclerView, dabirvaData: DabirvaData) {
+    val currentAdapter: RecyclerView.Adapter<*>? = recyclerView.adapter
+    if (currentAdapter == null) {
+        recyclerView.adapter = Dabirva(dabirvaData)
+    } else if (currentAdapter is Dabirva) {
+        currentAdapter.data = dabirvaData
+    } else {
+        throw IllegalStateException("Different adapter already set")
     }
 }
