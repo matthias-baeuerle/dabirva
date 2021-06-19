3c579c76f68692faf2c4001824d286c72bbe91ff
Matthias Bäuerle <Ma.Baeuerle@gmx.de>
2021-06-19 16:02:01 +0200

    Improve test

--- example/src/androidTest/java/com/matbadev/dabirva/example/ItemDiffingInstrumentedTest.kt
+++ example/src/androidTest/java/com/matbadev/dabirva/example/ItemDiffingInstrumentedTest.kt
@@ -1,11 +1,14 @@
 package com.matbadev.dabirva.example
 
+import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.AdapterDataObserver
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.matbadev.dabirva.DabirvaData
 import com.matbadev.dabirva.example.NoteViewModels.A
@@ -18,7 +21,6 @@ import com.matbadev.dabirva.example.ui.diffing.ItemDiffingActivity
 import com.matbadev.dabirva.example.ui.diffing.ItemDiffingActivityViewModel
 import com.matbadev.dabirva.example.util.DataBindingIdlingResourceRule
 import com.matbadev.dabirva.example.util.atViewPosition
-import com.matbadev.dabirva.example.util.registerAdapterDataObserver
 import com.matbadev.dabirva.example.util.useActivity
 import com.matbadev.dabirva.example.util.withChildCount
 import org.junit.Before
@@ -42,6 +44,12 @@ class ItemDiffingInstrumentedTest {
     @get:Rule
     val mockitoRule: MockitoRule = MockitoJUnit.rule().strictness(Strictness.STRICT_STUBS)
 
+    @get:Rule
+    val activityScenarioRule: ActivityScenarioRule<ItemDiffingActivity> = activityScenarioRule()
+
+    private val scenario: ActivityScenario<ItemDiffingActivity>
+        get() = activityScenarioRule.scenario
+
     @Mock
     private lateinit var adapterDataObserver: AdapterDataObserver
 
@@ -49,7 +57,6 @@ class ItemDiffingInstrumentedTest {
 
     @Before
     fun prepare() {
-        val scenario = ActivityScenario.launch(ItemDiffingActivity::class.java)
         dataBindingIdlingResourceRule.setScenario(scenario)
         viewModel = scenario.useActivity { it.viewModel }
     }
@@ -78,31 +85,39 @@ class ItemDiffingInstrumentedTest {
     }
 
     private fun runTest(initialItems: List<NoteViewModel>, updatedItems: List<NoteViewModel>) {
-        onView(withId(R.id.recycler_view)) //
-            .check(matches(withChildCount(0))) //
+        checkRecyclerViewItems(listOf())
 
         viewModel.dabirvaData.value = DabirvaData(
             items = initialItems,
         )
 
+        checkRecyclerViewItems(initialItems)
+
+        runWithAdapterDataObserver {
+            viewModel.dabirvaData.value = DabirvaData(
+                items = updatedItems,
+            )
+            checkRecyclerViewItems(updatedItems)
+        }
+    }
+
+    private fun checkRecyclerViewItems(expectedItems: List<NoteViewModel>) {
         onView(withId(R.id.recycler_view)) //
-            .check(matches(withChildCount(initialItems.size)))
-        initialItems.forEachIndexed { index, noteViewModel ->
+            .check(matches(withChildCount(expectedItems.size)))
+        expectedItems.forEachIndexed { index, noteViewModel ->
             onView(atViewPosition(R.id.recycler_view, index)) //
                 .check(matches(withText(noteViewModel.text)))
         }
-        onView(withId(R.id.recycler_view)) //
-            .perform(registerAdapterDataObserver(adapterDataObserver))
+    }
 
-        viewModel.dabirvaData.value = DabirvaData(
-            items = updatedItems,
-        )
-
-        onView(withId(R.id.recycler_view)) //
-            .check(matches(withChildCount(updatedItems.size)))
-        updatedItems.forEachIndexed { index, noteViewModel ->
-            onView(atViewPosition(R.id.recycler_view, index)) //
-                .check(matches(withText(noteViewModel.text)))
+    private fun runWithAdapterDataObserver(block: () -> Unit) {
+        val recyclerView: RecyclerView = scenario.useActivity { it.findViewById(R.id.recycler_view) }
+        val recyclerViewAdapter = checkNotNull(recyclerView.adapter)
+        recyclerViewAdapter.registerAdapterDataObserver(adapterDataObserver)
+        try {
+            block()
+        } finally {
+            recyclerViewAdapter.unregisterAdapterDataObserver(adapterDataObserver)
         }
     }
 
--- example/src/androidTest/java/com/matbadev/dabirva/example/util/RecyclerViewActions.kt
+++ example/src/androidTest/java/com/matbadev/dabirva/example/util/RecyclerViewActions.kt
@@ -3,20 +3,14 @@ package com.matbadev.dabirva.example.util
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.AdapterDataObserver
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
-import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import org.hamcrest.Matcher
 import org.hamcrest.Matchers.allOf
 
 fun scrollToWithOffset(position: Int, offset: Int = 0): ViewAction {
     return OffsetScrollRecyclerViewAction(position, offset)
-}
-
-fun registerAdapterDataObserver(observer: AdapterDataObserver): ViewAction {
-    return RegisterAdapterDataObserver(observer)
 }
 
 private class OffsetScrollRecyclerViewAction(
@@ -37,26 +31,6 @@ private class OffsetScrollRecyclerViewAction(
         val layoutManager = recyclerView.layoutManager as LinearLayoutManager
         layoutManager.scrollToPositionWithOffset(position, offset)
         uiController.loopMainThreadUntilIdle()
-    }
-
-}
-
-private class RegisterAdapterDataObserver(
-    private val observer: AdapterDataObserver,
-) : ViewAction {
-
-    override fun getConstraints(): Matcher<View> {
-        return allOf(isDisplayed(), isAssignableFrom(RecyclerView::class.java))
-    }
-
-    override fun getDescription(): String {
-        return "register an AdapterDataObserver"
-    }
-
-    override fun perform(uiController: UiController, view: View) {
-        val recyclerView = view as RecyclerView
-        val adapter: RecyclerView.Adapter<*> = recyclerView.adapter!!
-        adapter.registerAdapterDataObserver(observer)
     }
 
 }
