bb92aace6616e5e30a6a8f9e305040a5e87b2593
Matthias Bäuerle <ma.baeuerle@gmx.de>
2021-06-20 13:14:58 +0200

    Fix IllegalStateException in DataBindingViewHolder (#15)

--- dabirva/src/main/java/com/matbadev/dabirva/DataBindingViewHolder.kt
+++ dabirva/src/main/java/com/matbadev/dabirva/DataBindingViewHolder.kt
@@ -1,31 +1,27 @@
 package com.matbadev.dabirva
 
+import androidx.annotation.MainThread
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
-import java.util.concurrent.atomic.AtomicReference
 
 class DataBindingViewHolder(
     private val binding: ViewDataBinding,
 ) : RecyclerView.ViewHolder(binding.root) {
 
-    private val boundViewModelRef: AtomicReference<ItemViewModel?> = AtomicReference()
+    var boundViewModel: ItemViewModel? = null
+        private set
 
-    val boundViewModel: ItemViewModel?
-        get() = boundViewModelRef.get()
-
+    @MainThread
     fun bindViewModel(viewModel: ItemViewModel) {
-        if (boundViewModelRef.compareAndSet(null, viewModel)) {
-            binding.setVariable(viewModel.bindingId, viewModel)
-            binding.executePendingBindings()
-        } else {
-            throw IllegalStateException("Already bound")
-        }
+        binding.setVariable(viewModel.bindingId, viewModel)
+        binding.executePendingBindings()
+        boundViewModel = viewModel
     }
 
+    @MainThread
     fun unbind() {
-        if (boundViewModelRef.getAndSet(null) != null) {
-            binding.unbind()
-        }
+        boundViewModel = null
+        binding.unbind()
     }
 
 }
