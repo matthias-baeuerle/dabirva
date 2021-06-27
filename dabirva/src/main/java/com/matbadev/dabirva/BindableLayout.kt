bae54d604daf8d95ae4b80f9a7515ae08c4ddaf7
Matthias Bäuerle <Ma.Baeuerle@gmx.de>
2021-06-27 23:40:10 +0200

    Add code docs

--- dabirva/src/main/java/com/matbadev/dabirva/BindableLayout.kt
+++ dabirva/src/main/java/com/matbadev/dabirva/BindableLayout.kt
@@ -1,17 +1,47 @@
 package com.matbadev.dabirva
 
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
 /**
- * Contains everything necessary to bind a layout to a specific object using data binding.
+ * Contains everything necessary to bind a data object to a layout using data binding.
+ *
+ * The [layoutId] must specify a layout which defines a variable with a name that corresponds to [bindingId].
+ *
+ * In the context of [Dabirva] the [bindingId] is usually `BR.viewModel`
+ * and the layout defines the corresponding view model variable, e.g.:
+ *
+ * ```xml
+ * <layout xmlns:android="http://schemas.android.com/apk/res/android">
+ *   <data>
+ *     <variable
+ *       name="viewModel"
+ *       type="com.matbadev.dabirva.example.ui.NoteViewModel" />
+ *   </data>
+ *
+ *   <!-- View hierarchy -->
+ *
+ * </layout>
+ * ```
  */
 interface BindableLayout {
 
     /**
-     * ID of the layout stored in the generated R-class.
+     * Returns the ID of the layout stored in the generated `R` class.
+     *
+     * This is primarily used to inflate the specific layout.
+     *
+     * @see DataBindingUtil.inflate
      */
     val layoutId: Int
 
     /**
-     * The binding ID stored in the generated BR-class
+     * Returns the binding ID stored in the generated `BR` class.
+     *
+     * This is used to bind a data object to the layout with [layoutId].
+     * Therefore that layout must define a variable with the name that corresponds to the [bindingId].
+     *
+     * @see ViewDataBinding.setVariable
      */
     val bindingId: Int
 
--- dabirva/src/main/java/com/matbadev/dabirva/DabirvaData.kt
+++ dabirva/src/main/java/com/matbadev/dabirva/DabirvaData.kt
@@ -3,6 +3,16 @@ package com.matbadev.dabirva
 import androidx.recyclerview.widget.RecyclerView
 import java.util.concurrent.Executor
 
+/**
+ * Contains all data required by [Dabirva] to build a list.
+ *
+ * For each [RecyclerView] defined in a XML layout
+ * the corresponding view model should provide an instance of this class
+ * which should be bound to [Dabirva] using the binding adapter provided by [setDabirvaData].
+ *
+ * This class exists to be able to define and apply all list data at once
+ * instead of having to define multiple fields in the view model for a single list.
+ */
 data class DabirvaData(
     val items: List<ItemViewModel> = listOf(),
     val itemDecorations: List<RecyclerView.ItemDecoration> = listOf(),
